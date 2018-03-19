f7fd2bb7dd36e21b0b114e291b91f3c818b66ed2
Stefan Larsson <stefan@ngti.nl>
2018-03-19 20:42:17 +0100

    Added missing ipopt install dir parameter

--- install-ubuntu-trusty.sh
+++ install-ubuntu-trusty.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y gfortran unzip
 wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.7.zip
 unzip Ipopt-3.12.7.zip
 rm Ipopt-3.12.7.zip
-sudo ./install_ipopt.sh
+sudo ./install_ipopt.sh Ipopt-3.12.7
 
 # CppAD
 sudo apt-get install -y cppad
