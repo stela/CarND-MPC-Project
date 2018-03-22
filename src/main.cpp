7e8494a16d7a7cfc62348547d6ad4bb33f4cf925
Stefan Larsson <stefan@larsson.nu>
2018-03-22 01:14:34 +0100

    Compiles again

--- src/main.cpp
+++ src/main.cpp
@@ -137,32 +137,36 @@ int main() {
             next_y_vals.push_back(polyeval(coeffs, poly_inc * i));
           }
 
-          // TODO finish me later
-
-
-
-          json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = throttle_value;
 
           //Display the MPC predicted trajectory 
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
 
+          for (int i = 2; i < vars.size(); i++) {
+            if (i % 2 == 0) {
+              mpc_x_vals.push_back(vars[i]);
+            } else {
+              mpc_y_vals.push_back(vars[i]);
+            }
+          }
+
+          double Lf = 2.67;
+
+          json msgJson;
+
+          msgJson["steering_angle"] = vars[0]/deg2rad(25) * Lf;
+          msgJson["throttle"] = vars[1];
+
+
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
-
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
 
-          //Display the waypoints/reference line
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
-
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-          // the points in the simulator are connected by a Yellow line
+          msgJson["next_x"] = next_x_vals;
+          msgJson["next_u"] = next_y_vals;
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
